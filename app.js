4c940e94414c3916c1e48de97ecc75de2cefc97e
Humasahar-dev <125361368+Humasahar-dev@users.noreply.github.com>
2023-05-16 00:10:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+// function myData(e){
+//     e.preventDefault()
+    
+//     // console.log ("run")
+//    var userName =document.getElementById('username').value
+//    var email =document.getElementById('email').value
+//    var domain =document.getElementById('domain').value
+//    console.log (userName,email,domain)
+   
+// // 0rrr
+// var useremail =email+domain
+// console.log (email, domain ,userName)
+
+
+
+// }
+  // function usercontact(e){
+  //   e.preventDefault()
+  //   var countrycode =document.getElementById('countrycode').value
+  //  var companycode=document.getElementById('companycode').value
+  //  var customercode =document.getElementById('customercode').value
+
+  //  var contact =countrycode+companycode+customercode
+  //  console.log (contact)
+
+
+  //  document.getElementById(" output").value
+
+
+
+
+ 
+  // }
+// Not work
+//   function converter(e) {
+//     e.preventDefault()
+//     var converter=document.getElementById("converter").value;
+//     var uservalue= document.getElementById( "uservalue").value;
+//     var result;
+//     console.log(converter)
+    
+
+
+//   switch( converter){
+//          case "CtoF":
+//      result=(uservalue * 9/5) + 32.
+//      break;
+
+ 
+//        case "FtoC":
+      
+//        result = (uservalue - 32) * 5/9.
+//        break;
+          
+//     case "DtoR":
+
+//     // multiply the number of degrees by π/180.
+//     result= uservalue*(Math.PI/180)
+//     break;
+
+
+  
+//     case "RtoD":
+//       // multiply the number of degrees by 180π/.
+
+//    result =uservalue*(180/ Math.PI)
+//       break;
+
+//     default:
+//       break;
+  
+    
+// }
+//     console.log(result) 
+
+//     document.getElementById("output").value=result
+
+// }    
+
+
+
+// not work
+
+function readMore(){
+  var para =document.getElementById("mypara").innerHTML
+  console.log(para)
+
+}
+// var para =document.getElementById("mypara").innerHTML+= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti delectus nobis temporibus voluptates mollitia ex deserunt similique consectetur, obcaecati fugiat illo et sed consequuntur, error rerum nam consequatur officiis autem?"
